5fdf78ab9e6c448bea0a87eba3eb312ca65b3b48
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2021-04-13 12:42:21 +0300

    Initial Commit

--- navariapp/settings.py
+++ navariapp/settings.py
@@ -155,10 +155,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
-from decouple import config
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}

007ca350101820fefe6c6f5ab99eb1d25f3b5900
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2021-04-15 02:23:01 +0300

    Changes on the App

--- users/views.py
+++ users/views.py
@@ -4,17 +4,18 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileUpdateForm, UserRegisterForm, UserUpdateForm, UserCreationForm
 
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f' Your Account has been Created login now { username }!')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
-        form =UserRegisterForm()
-    return render(request, 'users/register.html',{'form': form})
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
 @login_required
 def profile(request):

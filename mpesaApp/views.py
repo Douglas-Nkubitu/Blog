7eeea76efbaab91bdd572e4228ff668b1dc4a1af
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2021-04-17 02:00:36 +0300

    MpesaApp

--- mpesaApp/views.py
+++ mpesaApp/views.py
@@ -86,6 +86,41 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
 
+def payment(request):
+    context = {
+        'payments': Mpesa_Payments.objects.all()
+    }
+    return render(request,'mpesaApp/payment.html', context )
+
+class Mpesa_PaymentsListView(ListView):
+    model = Mpesa_Payments
+    template_name = 'mpesaApp/payment.html'
+    context_object_name ='payments'
+    ordering = ['-created_at']
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def lipa_na_mpesa(request):
+    try:
+        req = json.loads(request.body.decode("utf-8"))
+        payment = Mpesa_Payments()
+        payment.MerchantRequestID = req['Body']['stkCallback']['MerchantRequestID']
+        payment.CheckoutRequestID = req['Body']['stkCallback']['CheckoutRequestID']
+        payment.Amount = req['Body']['stkCallback']['CallbackMetadata']['Item'][0]['Value']
+        payment.MpesaReceiptNumber = req['Body']['stkCallback']['CallbackMetadata']['Item'][1]['Value']
+        payment.TransactionDate = req['Body']['stkCallback']['CallbackMetadata']['Item'][3]['Value']
+        payment.PhoneNumber = req['Body']['stkCallback']['CallbackMetadata']['Item'][4]['Value']
+        payment.save()
+      
+    except:
+        pass
+    return JsonResponse({})
+
+def fetch_payments(request):
+    payment_list = list(Mpesa_Payments.objects.values('id','MerchantRequestID','CheckoutRequestID','Amount','MpesaReceiptNumber','TransactionDate','PhoneNumber','Status'))
+    return JsonResponse(payment_list,safe=False)
+
+
 
 def getAccessToken():
         consumer_key = 'cyBzERd2PMFDXlA6tMzcWbGwiCBYSMtn'
@@ -156,37 +191,3 @@ def Mpesa_Payments(request):
 
     form = MpesaForm()         
     return render (request, 'mpesaApp/mpesa_payments_form.html', {'form':form } )
-
-def payment(request):
-    context = {
-        'payments': Mpesa_Payments.objects.all()
-    }
-    return render(request,'mpesaApp/payment.html', context )
-
-class Mpesa_PaymentsListView(ListView):
-    model = Mpesa_Payments
-    template_name = 'mpesaApp/payment.html'
-    context_object_name ='payments'
-    ordering = ['-created_at']
-
-@csrf_exempt
-@require_http_methods(["POST"])
-def lipa_na_mpesa(request):
-    try:
-        req = json.loads(request.body.decode("utf-8"))
-        payment = Mpesa_Payments()
-        payment.MerchantRequestID = req['Body']['stkCallback']['MerchantRequestID']
-        payment.CheckoutRequestID = req['Body']['stkCallback']['CheckoutRequestID']
-        payment.Amount = req['Body']['stkCallback']['CallbackMetadata']['Item'][0]['Value']
-        payment.MpesaReceiptNumber = req['Body']['stkCallback']['CallbackMetadata']['Item'][1]['Value']
-        payment.TransactionDate = req['Body']['stkCallback']['CallbackMetadata']['Item'][3]['Value']
-        payment.PhoneNumber = req['Body']['stkCallback']['CallbackMetadata']['Item'][4]['Value']
-        payment.save()
-      
-    except:
-        pass
-    return JsonResponse({})
-
-def fetch_payments(request):
-    payment_list = list(Mpesa_Payments.objects.values('id','MerchantRequestID','CheckoutRequestID','Amount','MpesaReceiptNumber','TransactionDate','PhoneNumber','Status'))
-    return JsonResponse(payment_list,safe=False)

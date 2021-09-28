d3b0d9ef9c21eec46c7e02abbc5719c536fb625a
Douglas <nkubitudm@gmail.com>
2021-09-28 13:53:15 +0300

    Update settings.py

--- navariapp/settings.py
+++ navariapp/settings.py
@@ -80,27 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'navariapp.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'Database': 'd600ggrf42diih',
-#         'User': 'postgres',
-#         'Password': 'nkubitu',
-#         'Host': 'localhost',
-#         'Port': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
